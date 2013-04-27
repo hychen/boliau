fa5ee4dc13a0b9111e82ae415a1f2cc4c85af84b
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-04-27 20:07:17 +0800

    example: add a example that creating a report of launchpad bugtasks.

---
+++ example/gen_lp_report/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# find all bugtasks of laika project.
+boliau-lp-findbugtasks project laika > m1
+# find all bugtasks of bzr project which status is in progress.
+boliau-lp-findbugtasks project bzr --status 'In Progress' > m2
+# put outputs of m1 and m2 to a list.
+boliau-arr-combine m1 m2 > m3
+# use mako template to create a html.
+# -- mvar option means to assign the result provied from a mission which should be a Python object and and used in mako directly.
+# -- var optoin meaans to substitute by a string.
+boliau-tpl-sub report_tpl.html --mvar btsdata m3 --var title "Test Report" --output report_result.html
+
+# cleanup
+rm m1
+rm m2
+rm m3
